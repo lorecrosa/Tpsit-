d7d413c3e79421e95acb9e0f3e40c940aeef9e97
lorecrosa <crogamlollo@gmail.com>
2026-02-20 13:09:37 +0100

    Update social_post_it_parte1.js

--- Social_Post_It/social_post_it_parte1.js
+++ Social_Post_It/social_post_it_parte1.js
@@ -6,7 +6,6 @@ const DB_FILE = 'post.json';
 
 app.use(express.urlencoded({ extended: false }));
 
-// 1. Pagina con Form di invio e tasto Cancella
 app.get('/post', (req, res) => {
     res.send(`
         <form action="/post" method="POST">
@@ -20,7 +19,6 @@ app.get('/post', (req, res) => {
     `);
 });
 
-// 2. Salva i dati
 app.post('/post', (req, res) => {
     const dati = fs.existsSync(DB_FILE) ? JSON.parse(fs.readFileSync(DB_FILE, 'utf8')) : [];
     dati.push(req.body);
@@ -28,10 +26,10 @@ app.post('/post', (req, res) => {
     res.send('Dato salvato! <a href="/post">Torna indietro</a>');
 });
 
-// 3. Svuota il file JSON
 app.post('/clear', (req, res) => {
     fs.writeFileSync(DB_FILE, JSON.stringify([]));
     res.send('Tutti i dati sono stati cancellati! <a href="/post">Torna indietro</a>');
 });
+
 
 app.listen(3000, () => console.log('Server in ascolto su http://localhost:3000/post'));
